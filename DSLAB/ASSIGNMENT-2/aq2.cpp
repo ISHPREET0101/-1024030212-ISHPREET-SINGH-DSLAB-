f4c89edf4acc17decb8dbf5934839cefd157e2f5
ISHPREET0101 <s.ishpreet0177@gmail.com>
2025-08-19 11:29:27 +0530

    Update aq2.cpp

--- DSLAB/ASSIGNMENT-2/aq2.cpp
+++ DSLAB/ASSIGNMENT-2/aq2.cpp
@@ -6,37 +6,4 @@
 #include <string>
 using namespace std;
 
-bool substring(string s1, string s2) {
-    return s2.find(s1) != string::npos;
-}
-
-bool split(string s) {
-    int n = s.size();
-
-    for (int i = 1; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            string p1 = s.substr(0, i);
-            string p2 = s.substr(i, j - i);
-            string p3 = s.substr(j);
-
-            if ((substring(p1, p2) && substring(p1, p3)) ||
-                (substring(p2, p1) && substring(p2, p3)) ||
-                (substring(p3, p1) && substring(p3, p2))) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-int main() {
-    string s;
-    cout << "Enter string: ";
-    cin >> s;
-    if (split(s)){
-        cout << "It can be split.\n";
-    }else{
-        cout << "It cannot be split.\n";
-    }
-    return 0;
-}
+ 
