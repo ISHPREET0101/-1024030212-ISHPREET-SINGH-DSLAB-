07bbf379a20e92b49eb1765f8ea7839411899684
ISHPREET0101 <s.ishpreet0177@gmail.com>
2025-08-26 01:03:58 +0530

    Add files via upload

---
+++ DSLAB/Assignment-3/ques3.cpp
@@ -0,0 +1,42 @@
+// Write a program that checks if an expression has balanced parentheses. 
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isBalanced(string expr) {
+    stack<char> s;
+  
+    for (char ch : expr) {
+        if (ch == '(' || ch == '{' || ch == '[') {
+            s.push(ch);
+        }
+        
+        else if (ch == ')' || ch == '}' || ch == ']') {
+            if (s.empty()) return false;
+
+            char top = s.top();
+            s.pop();
+
+            if ((ch == ')' && top != '(') ||
+                (ch == '}' && top != '{') ||
+                (ch == ']' && top != '[')) {
+                return false;
+            }
+        }
+    }
+
+    return s.empty();
+}
+
+int main() {
+    string expr;
+    cout << "Enter an expression = ";
+    cin >> expr;  
+
+    if (isBalanced(expr))
+        cout << "Balanced expression " << endl;
+    else
+        cout << "Not balanced " << endl;
+
+    return 0;
+}
