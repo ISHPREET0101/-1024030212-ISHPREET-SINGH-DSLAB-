03b1e10e1ea68e966c599f289c0d2fa815c3944d
ISHPREET0101 <s.ishpreet0177@gmail.com>
2025-09-01 00:59:24 +0530

    Add files via upload

---
+++ DSLAB/Assignment-3/aq3.cpp
@@ -0,0 +1,37 @@
+// 3. Given an array arr[ ] of integers, the task is to find the Next Greater Element 
+// for each element of the array in order of their appearance in the array. Note:
+//  The Next Greater Element for an element x is the first greater element on the right 
+//  side of x in the array. Elements for which no greater element exist, consider the next 
+//  greater element as -1.  
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> large(vector<int> &arr){
+    int n = arr.size();
+    vector<int> arr1(n,-1);
+
+    for(int i=0;i<n;i++){
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[i]) {
+                arr1[i] = arr[j];
+                break;
+            }
+        }
+    }
+    return arr1;
+}
+
+
+int main() {
+
+    vector<int> arr = {6, 8, 0, 1, 3};
+    vector<int> arr1 = large(arr);
+    for (int x : arr1) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
+
