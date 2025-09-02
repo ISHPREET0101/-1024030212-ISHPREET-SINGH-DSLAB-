9fc0b91b21af4773e734813b3f61a8ffad4f9e8e
ISHPREET0101 <s.ishpreet0177@gmail.com>
2025-09-02 10:59:01 +0530

    Update aq2.cpp

--- DSLAB/ASSIGNMENT-2/aq2.cpp
+++ DSLAB/ASSIGNMENT-2/aq2.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 bool substring(string s) {
     int n = s.size();
-    for (int i = 1; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            string A = s.substr(0, i); //substr(pos,lenght)
-            string B = s.substr(i, j - i);
-            string C = s.substr(j);
+    for(int i=1; i<n - 1;i++) {
+        for (int j=i+ 1;j<n;j++) {
+            string A = s.substr(0,i); //substr(pos,lenght)
+            string B =s.substr(i, j-i);
+            string C =s.substr(j);
 
-            if ((B.find(A) != string::npos && C.find(A) != string::npos) ||
+            if((B.find(A) != string::npos && C.find(A) != string::npos) ||
                 (A.find(B) != string::npos && C.find(B) != string::npos) ||
                 (A.find(C) != string::npos && B.find(C) != string::npos)) {
                 return true;
@@ -24,10 +24,10 @@ bool substring(string s) {
 
 int main() {
     string s;
-    cout << "Enter string: ";
+    cout << "Enter string = ";
     cin >> s;
 
-    if (substring(s)) cout << "";
+    if(substring(s)) cout << "";
     else cout << "";
     return 0;
 }
