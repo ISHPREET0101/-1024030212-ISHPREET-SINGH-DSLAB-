4470cf8bb4f562db5f53130ffc6e5431c25737cb
ISHPREET0101 <s.ishpreet0177@gmail.com>
2025-09-02 10:59:54 +0530

    Update aq3.cpp

--- DSLAB/ASSIGNMENT-2/aq3.cpp
+++ DSLAB/ASSIGNMENT-2/aq3.cpp
@@ -7,16 +7,15 @@
 using namespace std;
 
 bool anagrams(string str1, string str2) {
-    if (str1.length() != str2.length())
+    if(str1.length() != str2.length())
         return false;
-
     sort(str1.begin(), str1.end());
     sort(str2.begin(), str2.end());
     return str1 == str2;
 }
 
 int main() {
-    string str1, str2;
+    string str1,str2;
     cout << "Enter first string = ";
     cin >> str1;
     cout << "Enter second string = ";
