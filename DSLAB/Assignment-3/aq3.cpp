a18925fd85d2ed708b152917438b39a94506f03d
ISHPREET0101 <s.ishpreet0177@gmail.com>
2025-09-02 10:50:16 +0530

    Update aq3.cpp

--- DSLAB/Assignment-3/aq3.cpp
+++ DSLAB/Assignment-3/aq3.cpp
@@ -11,9 +11,8 @@ using namespace std;
 vector<int>large(vector<int>&arr){
     int n = arr.size();
     vector<int>arr1(n,-1);
-
     for(int i=0;i<n;i++){
-        for (int j = i + 1; j < n; j++) {
+        for (int j=i + 1; j<n;j++) {
             if (arr[j] > arr[i]) {
                 arr1[i] = arr[j];
                 break;
@@ -26,8 +25,8 @@ vector<int>large(vector<int>&arr){
 
 int main() {
 
-    vector<int>arr = {6, 8, 0, 1, 3};
-    vector<int>arr1 = large(arr);
+    vector<int>arr ={6, 8, 0, 1, 3};
+    vector<int>arr1 =large(arr);
     for (int i=0;i<arr1.size();i++) {
         cout << i << " ";
     }
